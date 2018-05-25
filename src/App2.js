d362288e248be96afdc876720082366513ec2750
Marina Kun <kunmarina@gmail.com>
2018-05-25 13:51:50 -0400

    fixed?

--- src/App2.js
+++ src/App2.js
@@ -4,17 +4,17 @@ export default class App2 extends Component{
     render(){
         return(
         <div className='secondPage'>
-        <div className="box">
+            <div className="box">
             <h2>1. Enter Info</h2>
             <div className="labels">
-            <div class="foodLabel">
-                <label for="food">Food Name: </label>
-                <input type="text" name="food"></input></div>
-            <div className="amountLabel">
-                <label for="amount">Amount: </label>
-                <input type="text" name="amount"></input></div>
-          </div>
-          </div>
+                <div class="foodLabel">
+                    <label for="food">Food Name: </label>
+                    <input type="text" name="food"></input></div>
+                <div className="amountLabel">
+                    <label for="amount">Amount: </label>
+                    <input type="text" name="amount"></input></div>
+             </div>
+            </div>
          </div>
     );
     }
