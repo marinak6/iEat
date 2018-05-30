8916e55bc40e35650b96f6271b39053453ecf9ee
Caroline C <exokaroline@exokaroline.lan>
2018-05-30 16:13:09 -0400

    hello

--- src/App3.js
+++ src/App3.js
@@ -1,55 +1,67 @@
-// import React, { Component } from "react";
-// import firebase from "./configs";
-// import axios from "axios";
+import React, { Component } from "react";
+import firebase from "./configs";
+import axios from "axios";
 
-// var apiKey = "DEMO_KEY";
-// var ndbno = "01009";
-// var type = "b";
-// var format = "json";
-// var url = "http://api.nal.usda.gov/ndb/reports/?ndbno=" + ndbno + "&type=" + type + "&format=" + format + "&api_key=" + apiKey;
-// $.get(url, function( data ) {
-//     alert( "Data Loaded: " + JSON.stringify(data) );
-// });
 
-// //name + group + cf + ff + pf
-
-// export default class App3 extends Component{
-//     constructor(props){
-//         super(props)
-//         this.state = {
-//             name: "",
-//             amount: "",
-//             foods: [],
-//         }
-//     }
+export default class App3 extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            foodName: [],
+            foodGroup: [],
+            calories: [], 
+            fat: [],
+            protein: [] 
+        }
+    }
     
-    
-//     componentDidMount() {
-//         let url =
-//           "/src/USDA API" +
-//           API_KEY;
-    
-//         axios
-//           .get(url)
-//           .then(response => {
-//             this.setState({ ""
-//             });
-//           })
-//           .catch(err => {
-//             console.log(err);
-//           });
-//       }
-    
-// render() { 
+    componentDidMount() {
+            
+        const apiKey = "xfDET5R7EqwtYYaPcIdwa2DkKpf1jRzGXGJRFhsl";
+        const ndbno = document.getElementById();
+        const type = "b";
+        const format = "json";
+        const url = "https://api.nal.usda.gov/ndb/V2/reports/?ndbno=" + ndbno + "&type=" + type + "&format=" + format + "&api_key=" + apiKey;
+        $.get(url, function( data ) {
+            alert( "Data Loaded: " + JSON.stringify(data) );
+        });
 
-//     return(
-//         <div className='Background'>
-//          <div className="title">
-//             <h2>Diet Summary</h2></div>
-//         </div>
-//     )
-// }
-// }
+        axios
+        .get(url)
+        .then(response => {
+          this.setState({
+            foodName: response.data.results,
+            foodGroup: response.data.results,
+            calories: response.data.results,
+            fat: response.data.results,
+            protein: response.data.results,
+          });
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+    
+render() { 
 
+     let {foodName, foodGroup, calories, fat, protein} = this.state;
+     let points = foodName.map(x => {
+        return 
+        <div>
+        <li>{" "}Food:{x.foodName}{" "}</li>
+        <li>{" "}Group:{x.foodGroup}{" "}</li>    
+        <li>{" "}Calories:{x.calories}{" "}</li>    
+        <li>{" "}Fat:{x.fat}{" "}</li>    
+        <li>{" "}Protein:{x.protein}{" "}</li> 
+        </div>
+    });
 
-// export default App;
+    return(
+        <div className='Background'>
+         <div className="title">
+            <h2>Diet Summary</h2></div>
+            <ul>{points}</ul>
+        </div>
+    );
+}
+}
