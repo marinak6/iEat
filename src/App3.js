1636b3e8c2bad75b946e1fd3e96e601081e61f33
Marina Kun <kunmarina@gmail.com>
2018-05-30 16:22:04 -0400

    app3

--- src/App3.js
+++ src/App3.js
@@ -1,9 +1,6 @@
 import React, { Component } from "react";
 import firebase from "./configs";
 import axios from "axios";
-<<<<<<< Updated upstream
-
-=======
 import App2 from './App2.js'
 /*
 var apiKey = "DEMO_KEY";
@@ -16,74 +13,11 @@ $.get(url, function( data ) {
 });
 
 name + group + cf + ff + pf*/
->>>>>>> Stashed changes
 
 export default class App3 extends Component{
     constructor(props){
         super(props)
         this.state = {
-<<<<<<< Updated upstream
-            foodName: [],
-            foodGroup: [],
-            calories: [], 
-            fat: [],
-            protein: [] 
-        }
-    }
-    
-    componentDidMount() {
-            
-        const apiKey = "xfDET5R7EqwtYYaPcIdwa2DkKpf1jRzGXGJRFhsl";
-        const ndbno = document.getElementById();
-        const type = "b";
-        const format = "json";
-        const url = "https://api.nal.usda.gov/ndb/V2/reports/?ndbno=" + ndbno + "&type=" + type + "&format=" + format + "&api_key=" + apiKey;
-        $.get(url, function( data ) {
-            alert( "Data Loaded: " + JSON.stringify(data) );
-        });
-
-        axios
-        .get(url)
-        .then(response => {
-          this.setState({
-            foodName: response.data.results,
-            foodGroup: response.data.results,
-            calories: response.data.results,
-            fat: response.data.results,
-            protein: response.data.results,
-          });
-        })
-        .catch(err => {
-          console.log(err);
-        });
-    }
-    
-render() { 
-
-     let {foodName, foodGroup, calories, fat, protein} = this.state;
-     let points = foodName.map(x => {
-        return 
-        <div>
-        <li>{" "}Food:{x.foodName}{" "}</li>
-        <li>{" "}Group:{x.foodGroup}{" "}</li>    
-        <li>{" "}Calories:{x.calories}{" "}</li>    
-        <li>{" "}Fat:{x.fat}{" "}</li>    
-        <li>{" "}Protein:{x.protein}{" "}</li> 
-        </div>
-    });
-
-    return(
-        <div className='Background'>
-         <div className="title">
-            <h2>Diet Summary</h2></div>
-            <ul>{points}</ul>
-        </div>
-    );
-}
-}
-=======
-            name: "",
-            amount: "",
             foods: [],
         }
     }
@@ -141,4 +75,3 @@ render() {
 }
 }
 
->>>>>>> Stashed changes
